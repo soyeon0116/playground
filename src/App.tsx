e80d0dd8c0bb16691e50778369f356fc14d3804c
김소연 <ksy9252@naver.com>
2025-10-28 21:35:02 +0900

    feat : zod 연습

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,17 @@ import AppRoutes from './routes/AppRoutes';
 function App() {
   return (
     <>
-      <div>
-        <nav>
+      <div className='flex gap-3 h-screen'>
+        <nav className='w-44 border-r border-gray-500 p-2'>
           <h1>연습 리스트</h1>
-          <Link to='/Zod'>zod 연습</Link>
+          <Link to='/Zod' className='cursor-pointer hover:text-blue-500'>
+            1. zod 연습
+          </Link>
         </nav>
+        <div className='p-2'>
+          <AppRoutes />
+        </div>
       </div>
-      <AppRoutes />
     </>
   );
 }
--- src/pages/Zod.tsx
+++ src/pages/Zod.tsx
@@ -3,46 +3,83 @@
 import { useState } from 'react';
 import z from 'zod';
 
-const testZod = z.object({
-  name: z
-    .string()
-    .min(1, '이름은 필수로 입력해야합니다.')
-    .regex(/^[^0-9]+$/, '이름에는 숫자를 포함할 수 없습니다'),
-  age: z.number().min(1, '나이는 필수로 입력해야합니다.'),
-});
+const testZod = z
+  .object({
+    name: z
+      .string()
+      .min(4, '이름은 필수로 입력해야합니다.')
+      .regex(/^[^0-9]+$/, '이름에는 숫자를 포함할 수 없습니다'),
+    age: z.number().min(1, '나이는 필수로 입력해야합니다.'),
+    job: z.string().optional(),
+  })
+  .refine((data) => data.age < 20 || data.job, {
+    message: '20세 이상은 직업을 입력하세요',
+    path: ['job'],
+  });
 
 export default function Zod() {
   const [name, setName] = useState('');
   const [age, setAge] = useState('');
-  const [error, setError] = useState<string | null>(null);
+  const [job, setJob] = useState('');
+  const [errors, setErrors] = useState<Record<string, string>>({});
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     const result = testZod.safeParse({ name, age: Number(age) });
 
     if (result.success) {
-      setError(null);
+      setErrors({});
     } else {
-      console.log(result.error.issues[0]);
-      setError(result.error.issues[0].message);
+      let message: Record<string, string> = {};
+      result.error.issues.forEach((v) => {
+        const field = String(v.path[0]);
+        message[field] = v.message;
+      });
+      setErrors(message);
     }
+
+    // if (result.success) {
+    //   setErrors([]);
+    // } else {
+    //   let message = result.error.issues.map((issue) => issue.message);
+    //   setErrors(message);
+    // }
   };
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-3'>
         <input
           type='text'
+          placeholder='이름'
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
+        {errors.name && <p style={{ color: 'red' }}>{errors.name}</p>}
         <input
           type='number'
+          placeholder='나이'
           value={age}
           onChange={(e) => setAge(e.target.value)}
         />
+        {errors.age && <p style={{ color: 'red' }}>{errors.age}</p>}
+        {Number(age) >= 20 && (
+          <>
+            <input
+              type='text'
+              placeholder='직업'
+              value={job}
+              onChange={(e) => setJob(e.target.value)}
+            />
+            {errors.job && <p style={{ color: 'red' }}>{errors.job}</p>}
+          </>
+        )}
         <button type='submit'>submit</button>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
+        {/* {errors.map((v, i) => (
+          <p key={i} style={{ color: 'red' }}>
+            {v}
+          </p>
+        ))} */}
       </form>
     </div>
   );
