0c970150819ee058a06ba1aed9f25b3afd9b9307
김소연 <ksy9252@naver.com>
2025-10-30 16:27:10 +0900

    feat : /  페이지 추가

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className='text-gray-700 text-lg'>
+      👈 사이드바에서 연습 항목을 선택해보세요!
+    </div>
+  );
+}
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Zod from '../pages/Zod';
 import WebSocketChat from '../pages/WebSocket';
+import Home from '../pages/Home';
 
 export default function AppRoutes() {
   return (
     <Routes>
+      <Route path='/' element={<Home />} />
       <Route path='/Zod' element={<Zod />} />
-
       <Route path='/WebSocket' element={<WebSocketChat />} />
     </Routes>
   );
