e0bab607c121e1a1d6175436e9c35156548eea6e
김소연 <ksy9252@naver.com>
2025-10-31 13:47:57 +0900

    feat : jotai route추가

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Zod from '../pages/Zod';
 import WebSocketChat from '../pages/WebSocket';
 import Home from '../pages/Home';
+import Jotai from '../pages/Jotai';
 
 export default function AppRoutes() {
   return (
@@ -10,6 +11,7 @@ export default function AppRoutes() {
       <Route path='/playground' element={<Home />} />
       <Route path='/Zod' element={<Zod />} />
       <Route path='/WebSocket' element={<WebSocketChat />} />
+      <Route path='/Jotai' element={<Jotai />} />
     </Routes>
   );
 }
