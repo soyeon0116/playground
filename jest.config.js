fb311955614339dd08f9aa5cc4e9cbe03fcbebf2
김소연 <ksy9252@naver.com>
2025-11-11 10:01:19 +0900

    docs : jest 파일 추가

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: './tsconfig.json' }],
+  },
+};
