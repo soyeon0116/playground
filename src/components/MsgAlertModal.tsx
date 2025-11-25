edebc18aca136604b535a7cfb552c42a53fd30d9
김소연 <ksy9252@naver.com>
2025-11-25 17:04:47 +0900

    style : 얼럿 스타일 수정

--- src/components/MsgAlertModal.tsx
+++ src/components/MsgAlertModal.tsx
@@ -6,16 +6,16 @@ export default function MsgAlertModal() {
   const clearMsg = useMsgAlertStore((state) => state.clearMsg);
 
   return (
-    <div className='fixed bottom-4 right-4 bg-white shadow-lg border p-4 rounded-md'>
+    <div className='w-64 fixed bottom-4 right-4 bg-white shadow-lg border p-4 rounded-md'>
       <h3 className='font-bold mb-2'>📩 새 알림</h3>
       <ul>
         {messages.map((msg, idx) => (
-          <li key={idx} className='text-sm mb-1'>
+          <li key={idx} className='text-sm mb-1 truncate'>
             {msg}
           </li>
         ))}
       </ul>
-      <button className='mt-2 text-xs text-blue-500' onClick={clearMsg}>
+      <button className='mt-2 text-xs text-blue-500 w-full' onClick={clearMsg}>
         닫기
       </button>
     </div>
